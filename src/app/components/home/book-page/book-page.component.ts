c52e09b7ace5a3a008e9cb53a229f88be983e10c
juliannaventer <juliannaventer@gmail.com>
2024-04-09 12:11:00 +0200

    feat: :sparkles: Book page layout and readme

--- src/app/components/home/book-page/book-page.component.ts
+++ src/app/components/home/book-page/book-page.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './book-page.component.html',
-  styleUrl: './book-page.component.scss'
+  styleUrl: './book-page.component.scss',
 })
 export class BookPageComponent {
-
+  bookTitle =
+    'https://d3ui957tjb5bqd.cloudfront.net/uploads/2018/08/23061054/Classic-Book-Covers-Redesigned-4.png';
 }
