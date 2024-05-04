d4b8ecc8e0fff35382b9534591128f0d8f47b3bc
juliannaventer <juliannaventer@gmail.com>
2024-05-04 19:24:51 +0200

    feat: :fire: Removed pie charts

--- src/app/components/home/profile-stats/charts/charts.component.ts
+++ src/app/components/home/profile-stats/charts/charts.component.ts
@@ -1,24 +1,10 @@
-import { Component, ViewChild, inject } from '@angular/core';
-import { ChartComponent } from 'ng-apexcharts';
-
-import { NgApexchartsModule } from 'ng-apexcharts';
-import { ChartsDataService } from '../../../../services/charts-data.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-charts',
   standalone: true,
-  imports: [NgApexchartsModule],
+  imports: [],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.scss',
 })
-export class ChartsComponent {
-  @ViewChild('chart') chart: ChartComponent | undefined;
-  charts = inject(ChartsDataService);
-
-  chartOptionsPC = this.charts.chartOptionsPC;
-  chartOptionsTL = this.charts.chartOptionsTL;
-  chartOptionsDL = this.charts.chartOptionsDL;
-  chartOptionsIF = this.charts.chartOptionsIF;
-  chartOptionsAG = this.charts.chartOptionsAG;
-  chartOptionsRC = this.charts.chartOptionsRC;
-}
+export class ChartsComponent {}
