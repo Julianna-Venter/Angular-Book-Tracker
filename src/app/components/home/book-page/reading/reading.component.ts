ae5f770ef0d0aa2f8e2ce80179f9435d8bf22bc8
juliannaventer <juliannaventer@gmail.com>
2024-05-04 23:06:28 +0200

    feat: :sparkles: reading form

--- src/app/components/home/book-page/reading/reading.component.ts
+++ src/app/components/home/book-page/reading/reading.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { DnfComponent } from '../dnf/dnf.component';
 
 @Component({
   selector: 'app-reading',
   standalone: true,
-  imports: [DnfComponent],
+  imports: [DnfComponent, ReactiveFormsModule],
   templateUrl: './reading.component.html',
   styleUrl: './reading.component.scss',
 })
@@ -12,7 +13,58 @@ export class ReadingComponent {
   @Input() dnf!: boolean;
   @Output() completeReviewEvent = new EventEmitter<string>();
 
+  readingForm = new FormGroup({
+    rating: new FormControl(0),
+    ratingHalf: new FormControl(0),
+    rating1: new FormControl(0),
+    rating1Half: new FormControl(0),
+    rating2: new FormControl(0),
+    rating2Half: new FormControl(0),
+    rating3: new FormControl(0),
+    rating3Half: new FormControl(0),
+    rating4: new FormControl(0),
+    rating4Half: new FormControl(0),
+    rating5: new FormControl(0),
+    pace: new FormControl(0),
+    PC: new FormControl(0),
+    TL: new FormControl(0),
+    DL: new FormControl(0),
+    IF: new FormControl(0),
+    RC: new FormControl(0),
+    AG: new FormControl(0),
+    comments: new FormControl(''),
+  });
+
   completeReview() {
     this.completeReviewEvent.emit('read');
+    const rawForm = this.readingForm.getRawValue();
+
+    let rating = 0;
+
+    if (rawForm.rating1Half !== 0) {
+      rating = 0.5;
+    } else if (rawForm.rating1 !== 0) {
+      rating = 1;
+    } else if (rawForm.rating1Half !== 0) {
+      rating = 1.5;
+    } else if (rawForm.rating2 !== 0) {
+      rating = 2;
+    } else if (rawForm.rating2Half !== 0) {
+      rating = 2.5;
+    } else if (rawForm.rating3 !== 0) {
+      rating = 3;
+    } else if (rawForm.rating3Half !== 0) {
+      rating = 3.5;
+    } else if (rawForm.rating4 !== 0) {
+      rating = 4;
+    } else if (rawForm.rating4Half !== 0) {
+      rating = 4.5;
+    } else if (rawForm.rating5 !== 0) {
+      rating = 5;
+    } else {
+      rating = 0;
+    }
+
+    console.log(rawForm, typeof rating, rating);
   }
 }
