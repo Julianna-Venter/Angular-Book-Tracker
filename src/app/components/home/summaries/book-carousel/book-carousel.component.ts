6805293f52a8853bac702c5f5e61897fe2e7e533
juliannaventer <juliannaventer@gmail.com>
2024-05-09 19:18:15 +0200

    changes

--- src/app/components/home/summaries/book-carousel/book-carousel.component.ts
+++ src/app/components/home/summaries/book-carousel/book-carousel.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { UserDataState } from '../../../../store/reducers/user.reducer';
+import { selectGetBookList } from '../../../../store/selectors/user.selectors';
 
 @Component({
   selector: 'app-book-carousel',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './book-carousel.component.html',
   styleUrl: './book-carousel.component.scss',
 })
-export class BookCarouselComponent {}
+export class BookCarouselComponent {
+  userStore = inject(Store<UserDataState>);
+  bookList$ = this.userStore.select(selectGetBookList);
+
+  constructor() {
+    this.bookList$.subscribe((books) =>
+      console.log('carousel: ', typeof books)
+    );
+  }
+}
--- src/app/components/home/summaries/summaries.component.ts
+++ src/app/components/home/summaries/summaries.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { heroChevronDown } from '@ng-icons/heroicons/outline';
+import { Store } from '@ngrx/store';
+import { take } from 'rxjs';
+import { getBookList } from '../../../store/actions/user.actions';
+import { UserDataState } from '../../../store/reducers/user.reducer';
+import {
+  selectGetBookList,
+  selectgetUserData,
+} from '../../../store/selectors/user.selectors';
 import { SpotlightComponent } from '../../shared-components/spotlight/spotlight.component';
 import { BookAlbumComponent } from './book-album/book-album.component';
 import { BookCarouselComponent } from './book-carousel/book-carousel.component';
@@ -15,17 +24,24 @@ import { BookCarouselComponent } from './book-carousel/book-carousel.component';
     BookCarouselComponent,
     BookAlbumComponent,
     SpotlightComponent,
+    AsyncPipe,
   ],
   templateUrl: './summaries.component.html',
   styleUrl: './summaries.component.scss',
   viewProviders: [provideIcons({ heroChevronDown })],
 })
-export class SummariesComponent {
-  tbrVar: string = 'tbr';
-  dnfVar: string = 'dnf';
-  readVar: string = 'read';
+export class SummariesComponent implements OnInit {
+  userStore = inject(Store<UserDataState>);
+  users$ = this.userStore.select(selectgetUserData);
+  bookList$ = this.userStore.select(selectGetBookList);
 
-  constructor() {
-    console.log('SummariesComponent created');
+  ngOnInit(): void {
+    this.users$.pipe(take(2)).subscribe((users) => {
+      if (users && users[0] !== undefined && users[0].email) {
+        this.userStore.dispatch(
+          getBookList({ user: users[0], list: 'reading' })
+        );
+      }
+    });
   }
 }
