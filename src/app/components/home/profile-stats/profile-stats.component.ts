e0492e217cab5a79273f7086e7c65cef9f39972c
juliannaventer <juliannaventer@gmail.com>
2024-05-10 07:16:17 +0200

    feat: :sparkles: Refresh state in stats

--- src/app/components/home/profile-stats/profile-stats.component.ts
+++ src/app/components/home/profile-stats/profile-stats.component.ts
@@ -3,16 +3,18 @@ import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { heroStarSolid } from '@ng-icons/heroicons/solid';
 import { ChartsComponent } from './charts/charts.component';
 
+import { AsyncPipe } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { take } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
 import { ChartsDataService } from '../../../services/charts-data.service';
-import { getUserStats } from '../../../store/actions/user.actions';
+import { getUserData, getUserStats } from '../../../store/actions/user.actions';
 import { UserDataState } from '../../../store/reducers/user.reducer';
 import {
   selectGetUserStats,
   selectgetUserData,
 } from '../../../store/selectors/user.selectors';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile-stats',
@@ -30,7 +32,15 @@ export class ProfileStatsComponent implements OnInit {
   chartOptions = this.charts.polarChartOptions;
   barChartOptions = this.charts.barChartOptions;
 
+  constructor(private authService: AuthService) {}
+
   ngOnInit(): void {
+    this.authService.user$.pipe(take(1)).subscribe((user) => {
+      if (user) {
+        this.userStore.dispatch(getUserData({ email: user.email ?? '' }));
+      }
+    });
+
     this.user$.subscribe((user) => {
       if (user && user[0] !== undefined) {
         this.userStore.dispatch(getUserStats({ user: user[0] }));
